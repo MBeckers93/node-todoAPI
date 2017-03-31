33723271fd8fbb4fe9f8a22ba65877eef41f37cc
MBeckers93 <markbeckers11@hotmail.com>
2017-03-31 08:20:13 +0200

    Adding token method

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose'); 
+var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -89,6 +89,19 @@ app.patch('/todos/:id', (req, res) => {
 
 		res.send({todo});
 	}).catch((e) => res.status(400).send());
+});
+
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	})
 });
 
 app.listen(port, () => {
