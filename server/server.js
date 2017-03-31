c084ec048fcd048313cf465c3c6edb0a89159075
MBeckers93 <markbeckers11@hotmail.com>
2017-03-31 02:37:45 +0200

    Update automatic testing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-const {ObjectID} = require('mongodb');
-
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose'); 
 var {Todo} = require('./models/todo');
@@ -44,7 +43,7 @@ app.get('/todos/:id', (req, res) => {
 			return res.status(404).send();
 		}
 
-		res.status(200).send(todo);
+		res.send({todo});
 	}).catch((e) => res.status(400).send());
 });
 
